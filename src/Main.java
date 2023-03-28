c347003ef71ad4242c78c90062a7390a57c94147
he2794 <ehsani.fly@gmail.com>
2023-03-28 19:12:37 -0400

    the basic poeration model of the solar system

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+     //create solar system
+    solarSystem solar=new solarSystem();
+    solar.sun="1";
+    solar.moon="2";
+    solar.planets="2";
+
+
+
+    }
+}
